729c701edfa965a0bc508f44884b9269623eaaf5
Mirko Basic <bejzik8@gmail.com>
2023-12-21 08:35:35 +0100

    Testing variables... 🍀

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let bunnies = 2;
+    let typed: i32 = 2;
+    let (carrots, lettuce) = (14, 6);
+
+    let mut bunny = 3;
+    bunny = 4;
+
+    // bunnies = 45;
+
+    const BUNNIES_CONSTANT: f64 = 3.14;
 }
