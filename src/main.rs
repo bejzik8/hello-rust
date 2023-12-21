d302658ba0f5b25d55f4d6a602838fcf8d58ad88
Mirko Basic <bejzik8@gmail.com>
2023-12-21 08:47:36 +0100

    Scope and shadowing... 🍀

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,27 @@ fn main() {
     // bunnies = 45;
 
     const BUNNIES_CONSTANT: f64 = 3.14;
+
+    let x = 5;
+
+    {
+        let y = 6;
+        println!("{}, {}", x, y);
+    }
+
+    // println!("{}, {}", x, y); // Error
+
+    {
+        let x = 7;
+        println!("{}", x); // Prints 7
+    }
+
+    println!("{}", x); // Prints 5
+
+    // Shadowing
+    let mut z = 3;
+    let z = z;
+
+    // let meme = 'Something';
+    // let meme = make_image(meme);
 }
